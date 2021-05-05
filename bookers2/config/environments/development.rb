03b389768679089ba1c6f59d1e216058e88ec062
tmy628 <tmy6h1r@gmail.com>
2021-05-05 03:19:27 +0000

    Gmail用にAction Mailerを設定

--- bookers2/config/environments/development.rb
+++ bookers2/config/environments/development.rb
@@ -58,4 +58,16 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  #ActionController-Gmail
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :domain => 'gmail.com',
+    :authentication => :plain,
+    :user_name => Rails.application.credentials.gmail[:mail_address],
+    :password => Rails.application.credentials.gmail[:app_password]
+  }
 end

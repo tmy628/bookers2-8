c34b5b70fb825e84e26a0f91c83e5a9af93b4bd2
tmy628 <tmy6h1r@gmail.com>
2021-05-05 00:13:05 +0000

    [add] thanksメイラーにメソッドを追加

--- bookers2/app/mailers/thanks_mailer.rb
+++ bookers2/app/mailers/thanks_mailer.rb
@@ -1,2 +1,10 @@
 class ThanksMailer < ApplicationMailer
+  default from: 'from@example.com'
+
+  def thanks_email
+    @user = params[:user]
+    mail(to: @user.email, subject: 'Thank you for your registration!')
+  end
 end
+
+# mail(to: 宛先, subject: '件名')
